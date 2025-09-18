14b466ffc83592b6b8721accafc0c09f9fd20bfa
unknown <yoon27@kmac.co.kr>
2025-09-18 15:19:13 +0900

    글꼴 변경

--- app2.py
+++ app2.py
@@ -13,7 +13,7 @@ merge_final = pd.read_excel("0917대학역량측면데이터.xlsx")
 # -----------------------------
 # 폰트 설정
 # -----------------------------
-font_path = "KoPubWorld Dotum_Pro Medium.otf"  # 앱과 같은 폴더에 폰트 넣어두기
+font_path = "KoPubWorld Dotum Medium.ttf"  # 앱과 같은 폴더에 폰트 넣어두기
 font_path2 = "KoPubWorld Dotum Bold.ttf"
 font_name = fm.FontProperties(fname=font_path).get_name()
 plt.rc('font', family=font_name)
